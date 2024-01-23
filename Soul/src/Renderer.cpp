f9e2e07f45830e2db543626974186ab516f19b26
Tiago Pereira <tiagomaiapereira900@gmail.com>
2024-01-23 17:30:21 +0000

    Fixed Linker Error. Renderer initializes.

--- Soul/src/Renderer.cpp
+++ Soul/src/Renderer.cpp
@@ -1,44 +1,45 @@
 #include "spch.h"
 
-Soul::Renderer::Renderer(){}
+namespace Soul {
 
-Soul::Renderer::~Renderer(){}
+	SDL_Renderer* Renderer::renderTarget;
 
-void Soul::Renderer::Init(SDL_Window* window)
-{
-	if (window != nullptr) 
+	void Renderer::Init(SDL_Window* window)
 	{
-		renderTarget = SDL_CreateRenderer(window, NULL, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-
-		if (renderTarget == nullptr) 
+		if (window != nullptr)
 		{
-			std::cout << "[ERROR]: Failed to create render target! Renderer is null!" << std::endl;
+			renderTarget = SDL_CreateRenderer(window, NULL, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+			if (renderTarget == nullptr)
+			{
+				std::cout << "[ERROR]: Failed to create render target! Renderer is null!" << std::endl;
+			}
+			else
+			{
+				std::cout << "Renderer created!" << std::endl;
+			}
 		}
-		else 
+		else
 		{
-			std::cout << "Renderer created!" << std::endl;
+			std::cout << "[ERROR]: Target window is null!" << std::endl;
 		}
 	}
-	else
-	{
-		std::cout << "[ERROR]: Target window is null!" << std::endl;
-	}
-}
 
-SDL_Texture* Soul::Renderer::LoadTexture(std::string filePath)
-{
-	
-	SDL_Texture* texture = nullptr;
-	SDL_Surface* surface = SDL_LoadBMP(filePath.c_str());
-	if (surface == NULL)
-		std::cout << "[Error]: Failed to load the image: " << filePath << std::endl;
-	else
+	SDL_Texture* Renderer::LoadTexture(std::string filePath)
 	{
-		texture = SDL_CreateTextureFromSurface(renderTarget, surface);
-		if (texture == NULL)
-			std::cout << "Error" << std::endl;
-	}
-	SDL_FreeSurface(surface);
 
-	return texture;
+		SDL_Texture* texture = nullptr;
+		SDL_Surface* surface = SDL_LoadBMP(filePath.c_str());
+		if (surface == NULL)
+			std::cout << "[Error]: Failed to load the image: " << filePath << std::endl;
+		else
+		{
+			texture = SDL_CreateTextureFromSurface(renderTarget, surface);
+			if (texture == NULL)
+				std::cout << "Error" << std::endl;
+		}
+		SDL_FreeSurface(surface);
+
+		return texture;
+	}
 }
